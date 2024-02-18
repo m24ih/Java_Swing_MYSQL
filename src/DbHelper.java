a56a912a7af6fbaae9bd3bbbeb0a8bf68395de3a
Melih Ak <melihgaming67@gmail.com>
2024-02-18 14:07:58 +0300

    My First Swing App JDBC

---
+++ src/DbHelper.java
@@ -0,0 +1,14 @@
+import java.sql.*;
+public class DbHelper {
+    private String userName = "root";
+    private String password = "1928";
+    private String dbUrl = "jdbc:mysql://localhost:3306/world";
+    
+    public Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(dbUrl,userName,password);
+    }
+    public void showErrorMessage(SQLException exception){
+        System.out.println("Error: "+exception.getMessage());
+        System.out.println("Error Code: "+exception.getErrorCode());
+    }
+}
